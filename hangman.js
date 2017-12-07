1657efe38d9455c1b94eee203ec350d03c78d4fe
jkarlin929 <osida11572@gmail.com>
2017-12-07 18:59:23 -0500

    added pseudocode for js file

--- hangman.js
+++ hangman.js
@@ -1,1 +1,18 @@
-console.log("Here we go");
+// console.log("Here we go");
+//get input from form and use .val to store it in top-left id
+
+//use innerHTML from .val to put it into top-left id
+
+//maybe a start button?
+//add click event for each letter
+//set variable to create array of words as possible solutions
+//randomize through array of words and set that to randomized solutions variable
+//split randomized word into characters(maybe?)
+//if guessed letter is a match manipulate display css property from hidden to inline/block if character is in solution
+//after each click event, if letter is in solution use innerHTML to make it appear in solution
+//if letter is not in solution, animate the div of that letter disappear and number of turns decreases by one
+//if turns is 0 then indicate game over
+//have the letter change once it is clicked so the user cannot use it again
+//if number of turns is over (At 0) then innerHTML appears(game over)
+//if the solution is solved(all innerHTML matches the solution) then display you've won!
+//write a function to check for a winner to see if all divs have a display of block instead of hidden OR set a variable that holds the total number of correct guesses and check if that number is equal to the total number of letters in words
