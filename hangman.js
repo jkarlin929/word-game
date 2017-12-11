6940e22e9c4eb5d0904875cf2d9989a885ecf423
jkarlin929 <osida11572@gmail.com>
2017-12-11 18:45:03 -0500

    added a win function

--- hangman.js
+++ hangman.js
@@ -1,13 +1,15 @@
 $('document').ready(function() {// console.log("Here we go");
 
 //set form
+//id from form element
 const form = $('#form-entry');
-const submitButton = $('[type=button');
+//id from input element in form
+const nameSet = $('#name')
 form.submit(grabForm);
 function grabForm(event) {
   event.preventDefault();
-  const value = $('input')
-  value.val('');
+  const value = $('#player-name');
+  nameSet.html(value.val());
 }
 
 //get input from form and use .val to store it in top-left id
@@ -15,7 +17,7 @@ function grabForm(event) {
 //maybe a start button?
 //set variable to create array of words as possible solutions
 let turnsRemaining = 7;
-let words = ["cosmos", "rabbit", "valley", "flakes", "assign", "monkey", "forest"];
+let words = ["cosmos", "rabbit", "valley", "flakes", "assign", "monkey", "forest", "tissue", "office", "growth", "minute", "create", "button", "jaguar", "dinner", "museum", "permit", "search"];
 //randomize through array of words and set that to randomized solutions variable
 let solutions = words[Math.floor(Math.random() * words.length)];
 //split randomized word into characters
@@ -23,40 +25,54 @@ let characters = [...solutions];
 console.log(characters);
 const $spaces = $("#spaces");
 const $letters = $(".letters");
+let answersArray = [];
+let $turns = ('#turns');
 
   //add click event for each letter
   for (let j = 0; j < $letters.length; j++) {
-    $($letters[j]).click(function() {
+        $($letters[j]).click(function() {
       turnsRemaining -= 1
+      // console.log(turnsRemaining);
       //if my letter class is equal to a letter in the solution make innerHTML/display appear in spaces class
+        console.log($turns);
         let guessedLetter = $letters[j].innerHTML;
         let lowerCaseGuessed = guessedLetter.toLowerCase()
           $($letters[j]).css({'visibility': 'hidden'});
             if (characters.indexOf(lowerCaseGuessed) !== -1) {
         let indexOfLetter = characters.indexOf(lowerCaseGuessed);
-
           for (let k = 0; k < characters.length; k++) {
             if (characters[k] === lowerCaseGuessed) {
-              let answerSpaces = document.getElementsByClassName('answer-space');
-              console.log(answerSpaces);
-              answerSpaces[k].innerHTML = guessedLetter;
-              console.log(guessedLetter);
+
+              let $answerSpaces = $('.answer-space');
+              $($answerSpaces[k]).html(guessedLetter);
+              answersArray.push(guessedLetter);
             }
           }
         }
-      // checkWin;
+      checkWin(answersArray);
   })
 }
 //check for a winner by seeing if the letters in spaces class is equal to 6
-// function checkWin(){
-//   if(){
-//     alert("you did something!");
-//   }
+
+
+// class Word{
+//   constructor(word)
+//   this.word = word;
 // }
 
+// class Win {
+// constructor (word, wordLength)
+// this.word = word;
+// this.wordLength = wordLength;
+// this.turnsRemaining = 7;
+// this.correctLetter = 0;
+// }
 
-
-
+function checkWin(array) {
+  if (array.length === 6) {
+    alert('you won!')
+  }
+}
 
 // class Letter {
 //   constructor(value) {
@@ -85,9 +101,6 @@ const $letters = $(".letters");
 //   }
 // }
 
-// let word = new Word('hello')
-// word.createLetters()
-
 //create a button to reload the page
     let button = $("button");
   $(document).ready(function(){
@@ -105,4 +118,3 @@ const $letters = $(".letters");
 //if the solution is solved(all innerHTML matches the solution) then display you've won!
 //write a function to check for a winner to see if all divs have a display of block instead of hidden OR set a variable that holds the total number of correct guesses and check if that number is equal to the total number of letters in words
 })
-
